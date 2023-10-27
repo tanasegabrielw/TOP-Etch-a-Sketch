a320cb54d86368afd7ce6c046ab6294d92a470b5
Gabriel <tanase.gabriel671@gmail.com>
2023-10-27 11:32:13 +0300

    Add toggle function for mouseover
    I added the mouseover function outside of the loop, using event object( I need to spend more time understanding how event obejct works with functions
    
    I created a toggle eventListener to the main container by click so it
    toggles the drawing function

--- code.js
+++ code.js
@@ -1,24 +1,31 @@
 const pageContainer = document.querySelector(".container");
+isDrawing = false;
 
+function changeColor(event){
 
+    if (isDrawing){
+        event.target.classList.add("elementChanged");
+    }
+
+}
+pageContainer.addEventListener("click", ()=> {
+    isDrawing = !isDrawing;
+})
 
 for (let i = 0; i < 16; i++) {
+
     let gridDivColumn = document.createElement('div');
     pageContainer.appendChild(gridDivColumn);
 
 
-for (let j=0;j<16;j++){
+    for (let j=0;j<16;j++){
 
-   let gridDivRow = document.createElement('div');
-    gridDivRow.classList.add("gridElement");
-    gridDivColumn.appendChild(gridDivRow);
+        let gridDivRow = document.createElement('div');
+        gridDivRow.classList.add("gridElement");
+        gridDivColumn.appendChild(gridDivRow);
 
-    
-    gridDivRow.addEventListener("mouseover", () => {
-        gridDivRow.classList.add("elementChanged");})
-   
-}
-   
+        gridDivRow.addEventListener("mouseover", changeColor);
+    }
 }
 
 
