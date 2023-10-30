4bea17f8dfd664a97751ed9de80b8cce24c30e4f
Gabriel <tanase.gabriel671@gmail.com>
2023-10-30 10:03:46 +0200

    Add Reset feature for grid and limit for gird size

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
 const pageContainer = document.querySelector(".container");
+const btnGridSize = document.querySelector("#gridSizeBtn");
+const btnGridReset = document.querySelector("#resetGridBtn");
+const btnColor = document.querySelector("#color");
+let cellNumber=16;
 isDrawing = false;
 
 function changeColor(event){
@@ -8,27 +12,43 @@ function changeColor(event){
     }
 
 }
-pageContainer.addEventListener("click", ()=> {
-    isDrawing = !isDrawing;
+pageContainer.addEventListener("mousedown", ()=> {
+    isDrawing = !isDrawing; // toggles drawing on and off
 })
 
-for (let i = 0; i < 16; i++) {
+function createGrid(){
 
-    let gridDivColumn = document.createElement('div');
-    pageContainer.appendChild(gridDivColumn);
+    pageContainer.innerHTML="";
+    let numberOfCells = cellNumber * cellNumber;
 
+    for (let i = 0; i <numberOfCells; i++) {
 
-    for (let j=0;j<16;j++){
+        let cellElement = document.createElement('div');
+        cellElement.classList.add("gridElement");
+        pageContainer.appendChild(cellElement);
+        let borderOffSet = cellNumber*2; //removes the size of the border from the size of the cell
+        cellElement.style.width = ((800-borderOffSet)/cellNumber) + "px";
 
-        let gridDivRow = document.createElement('div');
-        gridDivRow.classList.add("gridElement");
-        gridDivColumn.appendChild(gridDivRow);
-
-        gridDivRow.addEventListener("mouseover", changeColor);
+        cellElement.addEventListener("mouseover", changeColor);
     }
 }
 
+btnGridSize.addEventListener("click",()=> {
+cellNumber= prompt("How many cells?",cellNumber);
 
+if (cellNumber>=50){
+    alert("Max 50!");
+
+}
+else{
+    createGrid();
+} 
+});
+
+btnGridReset.addEventListener("click", ()=> {
+createGrid();
+
+});
 
 
 
