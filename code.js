9757bcfc4d44e6e6d12e52357db4bb4b74d913e4
Gabriel <tanase.gabriel671@gmail.com>
2023-11-01 07:17:21 +0200

    Finished

--- code.js
+++ code.js
@@ -1,17 +1,27 @@
 const pageContainer = document.querySelector(".container");
-const btnGridSize = document.querySelector("#gridSizeBtn");
-const btnGridReset = document.querySelector("#resetGridBtn");
-const btnColor = document.querySelector("#color");
 let cellNumber=16;
 isDrawing = false;
+//buttons
+const btnGridSize = document.querySelector("#gridSizeBtn");
+const btnGridReset = document.querySelector("#resetGridBtn");
+const cellNumberInput = document.querySelector("#cellNumberInput");
+// Color picker
+let colorPicker;
+let currentColor = "#0000ff";
+window.addEventListener("load", startup, false);
+window.addEventListener("load", createGrid,false);
 
-function changeColor(event){
-
-    if (isDrawing){
-        event.target.classList.add("elementChanged");
-    }
-
+function startup(event) {
+colorPicker=document.querySelector("#color");
+colorPicker.value=currentColor; //initial color
+colorPicker.addEventListener("input",updateColor,false);
 }
+
+function updateColor(event) {
+//update currentColor when it changes value
+    currentColor=event.target.value;
+}
+
 pageContainer.addEventListener("mousedown", ()=> {
     isDrawing = !isDrawing; // toggles drawing on and off
 })
@@ -27,22 +37,21 @@ function createGrid(){
         cellElement.classList.add("gridElement");
         pageContainer.appendChild(cellElement);
         let borderOffSet = cellNumber*2; //removes the size of the border from the size of the cell
-        cellElement.style.width = ((800-borderOffSet)/cellNumber) + "px";
+        cellElement.style.width = ((640-borderOffSet)/cellNumber) + "px";
 
-        cellElement.addEventListener("mouseover", changeColor);
+        cellElement.addEventListener("mouseover", ()=>{
+            if(isDrawing){
+                cellElement.style.backgroundColor = currentColor;
+            }
+
+
+        });
     }
 }
 
 btnGridSize.addEventListener("click",()=> {
-cellNumber= prompt("How many cells?",cellNumber);
-
-if (cellNumber>=50){
-    alert("Max 50!");
-
-}
-else{
-    createGrid();
-} 
+    cellNumber= parseInt(cellNumberInput.value);
+    createGrid();   
 });
 
 btnGridReset.addEventListener("click", ()=> {
